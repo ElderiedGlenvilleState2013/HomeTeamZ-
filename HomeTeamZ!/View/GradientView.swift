ccbff9cfa54b7c776664b27c8edce6cb83258be9
McKinney family <mckinneyfamily@Macs-MacBook-Air.local>
2018-05-18 00:33:22 -0400

    lesson5

---
+++ HomeTeamZ!/View/GradientView.swift
@@ -0,0 +1,38 @@
+//
+//  GradientView.swift
+//  HomeTeamZ!
+//
+//  Created by McKinney family  on 5/18/18.
+//  Copyright © 2018 FasTek Technologies. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    @IBInspectable var bottomColor: UIColor = UIColor.green {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+       
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+    
+    
+    
+}
